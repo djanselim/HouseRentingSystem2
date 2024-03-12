b021bc31bc7ffd109688ee0deb83ad4f5b75c787
djanselim <114089140+djanselim@users.noreply.github.com>
2024-03-12 12:53:28 +0200

    Simplified Program.cs

---
+++ HouseRentingSystem2/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using HouseRentingSystem2.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtension
+	{
+		public static IServiceCollection AddApplicatonServices(this IServiceCollection services)
+		{
+			return services;
+		}
+
+		public static IServiceCollection AddApplicatonDbContext(this IServiceCollection services, IConfiguration config)
+		{
+			var connectionString = config.GetConnectionString("DefaultConnection");
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(connectionString));
+
+			services.AddDatabaseDeveloperPageExceptionFilter();
+
+			return services;
+		}
+		public static IServiceCollection AddApplicatonIdentity(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			return services;
+		}
+
+	}
+}
