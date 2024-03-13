65e411bfa9096da100b5fbfd250f012f65249397
djanselim <114089140+djanselim@users.noreply.github.com>
2024-03-13 12:07:05 +0200

    Database Created

---
+++ HouseRentingSystem.Infrastructure/Data/Models/House.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+	[Comment("House to rent")]
+	public class House
+	{
+		[Key]
+		[Comment("House Identifier")]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(HouseTitleMaxLength)]
+		[Comment("Title")]
+		public string Title { get; set; } = string.Empty;
+
+		[Required]
+		[MaxLength()]
+		[Comment("House address")]
+		public string Address { get; set; } = string.Empty;
+
+		[Required]
+		[MaxLength(HouseDescriptionMaxLength)]
+		[Comment("House description")]
+		public string Description { get; set; } = string.Empty;
+
+		[Required]
+		[Comment("House image url")]
+		public string ImageUrl { get; set; } = string.Empty;
+
+		[Required]
+		[Comment("Monthly Price")]
+		//[Range(typeof(decimal), HouseRentingPriceMin, HouseRentingPriceMax, ConvertValueInInvariantCulture = true)]
+		[Column(TypeName = "decimal(18, 2)")]
+		public decimal PricePerMonth { get; set; }
+
+		[Required]
+		[Comment("Category identifier")]
+		public int CategoryId { get; set; }
+
+		[Required]
+		[Comment("Agent identifier")]
+		public int AgentId { get; set; }
+
+		[Comment("User id of the renterer")]
+		public string? RenterId { get; set; } = string.Empty;
+
+		public Category Category { get; set; } = null!;
+
+		public Agent Agent { get; set; } = null!;
+	}
+}
--- HouseRentingSystem2/Extensions/ServiceCollectionExtension.cs
+++ HouseRentingSystem2/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,4 @@
-﻿using HouseRentingSystem2.Data;
+﻿using HouseRentingSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
 		public static IServiceCollection AddApplicatonDbContext(this IServiceCollection services, IConfiguration config)
 		{
 			var connectionString = config.GetConnectionString("DefaultConnection");
-			services.AddDbContext<ApplicationDbContext>(options =>
+			services.AddDbContext<HouseRentingDbContext>(options =>
 				options.UseSqlServer(connectionString));
 
 			services.AddDatabaseDeveloperPageExceptionFilter();
@@ -24,7 +24,7 @@ namespace Microsoft.Extensions.DependencyInjection
 		public static IServiceCollection AddApplicatonIdentity(this IServiceCollection services, IConfiguration config)
 		{
 			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-				.AddEntityFrameworkStores<ApplicationDbContext>();
+				.AddEntityFrameworkStores<HouseRentingDbContext>();
 
 			return services;
 		}
